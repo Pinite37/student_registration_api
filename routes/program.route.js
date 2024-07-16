75fb34b5a0164ca03fdb6e87bd05662628bb8658
Theophas <afomassetheophas@gmail.com>
2024-07-16 20:42:46 +0100

    commit

--- routes/program.route.js
+++ routes/program.route.js
@@ -4,7 +4,7 @@ const { addProgram, updateProgram, deleteProgram } = require('../controllers/pro
 const { authenticate, isAdmin } = require('../middlewares/auth.middleware');
 const { validate } = require('../middlewares/validations.middleware');
 
-router.post('/', authenticate, isAdmin, validate() addProgram);
+router.post('/', authenticate, isAdmin, validate(), addProgram);
 router.post('/:id', authenticate, validate(), updateProgram);
 router.delete('/:id', authenticate, isAdmin, deleteProgram);
 
